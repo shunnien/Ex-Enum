839ee4baa8ee6dfc0b25dff931efe28878ccb467
Allen Yu (FD-TW) <allen_yu@trend.com.tw>
2015-06-27 00:25:49 +0800

    testing wei's question

---
+++ Ex-Enum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ====== Start ======");
+
+            DBop _dBop = DBop.Update;
+
+            if (_dBop == DBop.Add)
+                Console.WriteLine(" if true");
+
+
+
+            Console.WriteLine(" ====== End ======");
+            Console.ReadKey();
+        }
+
+        internal enum DBop
+        {
+            no = 1,
+            Add = 2,
+            Update = 3,
+            Del = 4
+        };
+    }
+}
